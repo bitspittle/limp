192bba6c3959ab8cc20f19968aebba7d306ff015
David Herman <bitspittle@gmail.com>
2025-08-24 15:32:40 -0700

    Set Kotlin compat back to 1.5+

--- limp/build.gradle.kts
+++ limp/build.gradle.kts
@@ -6,12 +6,19 @@ plugins {
 group = "dev.bitspittle.limp"
 version = libs.versions.limp.lang.get()
 
+// Maximum compatibility
+val targetKotlinVersion = "1.5.32"
+
 kotlin {
     jvm()
     js {
         browser()
     }
     sourceSets {
+        commonMain.dependencies {
+            implementation(kotlin("stdlib", targetKotlinVersion))
+        }
+
         commonTest.dependencies {
             implementation(libs.kotlin.test.common)
             implementation(libs.kotlin.test.annotations.common)
